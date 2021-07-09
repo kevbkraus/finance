e2d0901665a722037b9465b112d38124f4c7a020
Dinesh Thogulua <saravanadinesh@gmail.com>
2021-07-09 22:57:02 +0530

    Saving code

--- usa/dcf_valuation.py
+++ usa/dcf_valuation.py
@@ -27,6 +27,7 @@ import requests
 import json
 import numpy as np
 import pandas as pd
+from scipy import stats
 
 import os
 import sys
@@ -150,7 +151,7 @@ if not (all(bsheet['fiscalDateEnding'] == incstmt['fiscalDateEnding']) and all(i
 # This is the starting point of actually valuing the company
 # NOTE: From this point on, we deal with Millions of $ 
 # --------------------------------------------------------------------------------------------------------------------------------
-fundamentals = pd.DataFrame(columns=['date', 'cash', 'equity', 'debt', 'revenue', 'r_and_d', 'opinc', 'intexp', 'netinc', 'netcapex', 'changeinwc'])
+fundamentals = pd.DataFrame(columns=['date', 'cash', 'equity', 'debt', 'revenue', 'RnD', 'opinc', 'intexp', 'netinc', 'netcapex', 'changeinwc'])
 
 fundamentals.loc[0, 'date'] = bsheetq.loc[0, 'fiscalDateEnding'] 
 fundamentals.loc[0, 'cash'] = bsheetq.loc[0, 'cashAndShortTermInvestments']/1E6 + bsheetq.loc[0, 'longTermInvestments']/1E6
@@ -161,7 +162,7 @@ if pd.isna(inc_ttm.['nonInterestIncome']) or inc_ttm['nonInterestIncome'] < 0:
 else:                                                               # some revenue related terms post 2017
     fundamentals.loc[0, 'revenue'] = inc_ttm['nonInterestIncome']/1E6
 
-fundamentals.loc[0, 'r_and_d'] = inc_ttm['researchAndDevelopment']/1E6
+fundamentals.loc[0, 'RnD'] = inc_ttm['researchAndDevelopment']/1E6
 fundamentals.loc[0, 'opinc'] = inc_ttm['operatingIncome']/1E6
 fundamentals.loc[0, 'intexp'] = inc_ttm['interestExpense']/1E6
 fundamentals.loc[0, 'netinc'] = inc_ttm['netIncome']/1E6
@@ -179,7 +180,7 @@ for i in range(0,len(bsheet.index)):
         fundamentals.loc[i+1, 'revenue'] = incstmt.loc[i, 'totalRevenue']/1E6
     else:    
         fundamentals.loc[i+1, 'revenue'] = incstmt.loc[i, 'nonInterestIncome']/1E6
-    fundamentals.loc[i+1, 'r_and_d'] = incstmt.loc[i, 'researchAndDevelopment']/1E6
+    fundamentals.loc[i+1, 'RnD'] = incstmt.loc[i, 'researchAndDevelopment']/1E6
     fundamentals.loc[i+1, 'opinc'] = incstmt.loc[i, 'operatingIncome']/1E6
     fundamentals.loc[i+1, 'intexp'] = incstmt.loc[i, 'interestExpense']/1E6
     fundamentals.loc[i+1, 'netinc'] = incstmt.loc[i, 'netIncome']/1E6
@@ -188,6 +189,79 @@ for i in range(0,len(bsheet.index)):
         fundamentals.loc[i+1, 'netcapex'] = cashflow.loc[i, 'capitalExpenditures']/1E6 - cashflow.loc[i, 'depreciationDepletionAndAmortization']/1E6
         fundamentals.loc[i+1, 'changeinwc'] = cashflow.loc[i, 'changeInInventory']/1E6 + cashflow.loc[i, 'changeInReceivables']/1E6 \
                                             - (bsheet.loc[i, 'currentAccountsPayable']/1E6 - bsheet.loc[i+1, 'currentAccountsPayable']/1E6)
+
+
+# Capitalize R&D (with 5 year straight line depreciation)
+# We find the average R&D expense and depreciate them as operating expense. The reason we do this is that we are going to find 
+# ROIC and reinvestment rates for DCF valuation by averaging figures over several years in the past. But, we won't be able to 
+# find out the depreciation values of one or more past R&D expenses for the majority of historic data. For example, if we use
+# 5 year historic data, and use 5 year straight line depreciation, no historic data can be used for fiding ROIC, reinvestment
+# rate, because for oldest 4 years, we cannot get accurate depreciated R&D values simply because we don't have R&D data from 
+# the past before the 5-years data we have
+if any fundamentals['RnD'] < 0: # Sanity check
+    print('Error: R&D expense cannot be negative')
+    sys.exit()
+
+average_RnD = fundamentals['RnD'].mean()
+RnD_asset = 3*average_RnD # 1+0.8+0.6+0.4+0.2 = 3
+RnD_depreciation = 0.8*average_RnD # R&D expense made one year is depreciated 20% for every year after that. 4*0.2 = 0.8
+
+fundamentals['equity'] = fundamentals['equity'] + RnD_asset
+fundamentals['opinc'] = fundamentals['opinc'] + fundamentals['RnD'] - RnD_depreciation
+fundamentals['netcapex'] = fundamentals['netcapex'] + fundamentals['RnD'] - RnD_depreciation 
+
+
+# Calculate ROIC
+invested_capitals = fundamentals['equity']+fundamentals['debt']-fundamentals['cash']
+roics = pd.Series(dtype=float)
+roics[0] = fundamentals.loc[0,'opinc']*(1-tax/100)*2/(invested_capitals[1]+invested_capitals[2]) # For calculating ROIC of TTM, we are taking a shortcut: We -
+                                                                                           # are using the average invested capital of the previous -
+                                                                                           # financial year and the current - instead of finding the 
+                                                                                           # BVs of equity, debt, cash of the correct quarter 12 months
+                                                                                           # ago, adjusting for R&D etc.
+
+iter_len = 5 if len(fundamentals) > 6 else len(fundamentals)-1 # We don't want to consider anything older than 5 years
+for i in range(1,iter_len): # We skip the last one as we don't have information about invested capital before the oldest year
+    roics[i] =  fundamentals[i,'opinc']*(1-tax/100)/invested_capitals.loc[i+1]
+
+roics_pos = pd.Series([x for x in roics if x>0]) # Extract only positive ROIC values
+if len(roics_pos) < 4: # We are overall examining 5 historic years and one ttm
+    print('Error: This company has too many loss years')
+    sys.exit()
+
+roic = stats.gmean(roics_pos.values)    # Geometric mean of positive ROIC values
+
+# Calculate reinvestment rate (rir)
+reinvestments = fundamentals['netcapex'] + fundamentals['changeinwc']
+rirs = pd.Series(dtype=float)
+free_index = 0
+for i in range(0, iter_len):
+    if reinvestments[i] < 0: # We don't want to consider negative reinvestments as it will make calculating average rir difficult
+        continue 
+    if fundamentals.loc[i,'opinc'] < 0: # We don't want to consider years where they incurred a loss
+        continue
+
+    rirs[free_index] = reinvestments[i]/(fundamentals.loc[i,'opinc']*(1-tax))
+    free_index = free_index+1
+
+if len(rirs) < 3: # We are overall examining 5 historic years and one ttm
+    rir = 0
+else:
+    rir = stats.gmean(rirs)
+
+# -------------------------------------------- Calculate Weighted Average Cost of Capital ---------------------------------------
+# We use synthetic bond rating to find cost of debt, and use BV of debt in place of MV of debt
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+# -------------------------------------------- DCF calculation -------------------------------------------------------------------
+# We use a growth ramp down model where we get from current growth to growth rate of economy (as decided by risk free rate, and 
+# then settle for growth at the rate of risk free rate
+# --------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
 
 
 
