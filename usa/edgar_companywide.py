42773f086136e77b36639d9b3e9e57d160c4240b
Dinesh Thogulua <saravanadinesh@gmail.com>
2024-03-11 10:58:49 -0400

    Added a function called get_companywide_concepts that returns, for several companies, several concepts for each company over all the years for which each concept has a record in EDGAR database

--- usa/edgar_companywide.py
+++ usa/edgar_companywide.py
@@ -1,10 +1,12 @@
 import pandas as pd
-import ssl
+import requests
+import json
 
-from edgar_utils import ticker_to_cik, get_financial_item
+
+from edgar_utils import ticker_to_cik, get_financial_item, get_companywide_concepts
 from adamodaran_utils import extract_industry_tickers
 
-# INPUTS
+
 # I use Prof. Damodaran's indname.xls to find companies that belong to an industry. You must enter industry name(s) -
 # exactly the way they are specified in indname.xls, sheet 'US by industry'. You can find the xl sheet here: -
 # https://www.stern.nyu.edu/~adamodar/pc/datasets/indname.xls
@@ -23,13 +25,16 @@ print("Code execution begins")
 # 10-digit CIK values for companies. So we use first extract the Ticker symbols of the companies that belong to -
 # the industries we are interested in and then use a mapping between Ticker symbols and CIK values from EDGAR 
 # website to create a list of CIK values
-ticker_list = extract_industry_tickers(industries_of_interest)
-ticker_list.sort()  # Sorting it so that we can manually verify if the cik values extracted for tickers are right. -
+
+#ticker_list = extract_industry_tickers(industries_of_interest)
+#ticker_list.sort()  # Sorting it so that we can manually verify if the cik values extracted for tickers are right. -
                     # This just makes debugging easy. It doesn't serve any functional purpose
 
 ticker_list = ['AAPL', 'MSFT','NVDA']
-cik_list = ticker_to_cik(ticker_list)
-financial_item_of_interest = "AccountsPayableCurrent"
-cik_str = cik_list[0]
-AccountsPayable_df = get_financial_item(cik_str, financial_item_of_interest)
+cik_str_list = ticker_to_cik(ticker_list)
+concepts_of_interest = ["AccountsPayableCurrent", "ResearchAndDevelopmentExpense"]
+
+all_companies_dict = get_companywide_concepts(cik_str_list, concepts_of_interest)
+
+
 print("Dummy")
--- usa/edgar_utils.py
+++ usa/edgar_utils.py
@@ -28,8 +28,10 @@ def ticker_to_cik(ticker_list):
     return cik_str_list
 
 # get_financial_item
+# Description
+#   Gets the values of a financial item for a company across time
 # Inputs
-#   cik_str: 10-digit CIK of the company as a string. Example: 0000320193 (for AAPL). Tip: To find the CIK of a company,
+#   cik_str: 10-digit CIK of the company as a string. Example: "0000320193" (for AAPL). Tip: To find the CIK of a company,
 #       go to https://www.sec.gov/edgar/searchedgar/companysearch, and type in the company name (ex. "Apple") and wait
 #       for the drop down suggestions show "Apple Inc. (AAPL)". The CIK no. is displayed next to the suggestion. If you 
 #       want to copy+paste the CIK, just click on the suggestion, it will take you to the Apple company page in 
@@ -42,6 +44,7 @@ def ticker_to_cik(ticker_list):
 # Output
 #   data_df: A data frame that contains quarterly, annual values of the financial_item_of_interest for several years
 
+
 def get_financial_item(cik_str, financial_item_of_interest):
     headers = {'User-Agent': 'Fordham University sthoguluachandraseka@fordham.edu', 'Accept-Encoding': 'gzip,deflate', 'Host':'data.sec.gov'}
     URL = "https://data.sec.gov/api/xbrl/companyconcept/CIK"+cik_str+"/us-gaap/"+ financial_item_of_interest+".json"
@@ -53,3 +56,42 @@ def get_financial_item(cik_str, financial_item_of_interest):
     
     data_df = pd.DataFrame.from_dict(data_dict['units']['USD'])
     return(data_df)
+
+
+# get_companywide_concepts(cik_str_list, concepts_of_interest)
+# Gets the values of a financial item for several companies (annual, not quarterly) across time
+# Inputs
+#   cik_str_list: A list of 10-digit CIK of companies as strings. Example: ["0000320193", "0001045810"] 
+#       Tip: See the description of get_financial_item to figure out how to find the CIK value of a company 
+#   concepts_of_interest: A list of IS/BS/CFS items in standard EDGAR terminology. 
+#       Example: ["AccountsPayableCurrent", "ResearchAndDevelopmentExpense"]
+#       Tip:  See the description of get_financial_item to figure out how to find standard term for a financial 
+#       item of interest
+# Output
+#   all_companies_dict = A dictionary that has a dataframe for each company with company CIK as the key. The dataframe 
+#   for each company has three columns: concept, end and val, 
+#   several years of data. 
+def get_companywide_concepts(cik_str_list, concepts_of_interest):
+    all_companies_dict = dict.fromkeys(cik_str_list, None)
+    for company in all_companies_dict:
+        headers = {'User-Agent': 'Fordham University sthoguluachandraseka@fordham.edu', 'Accept-Encoding': 'gzip,deflate', 'Host':'data.sec.gov'}
+        URL = "https://data.sec.gov/api/xbrl/companyfacts/CIK"+company+".json"
+        resp = requests.get(URL, headers=headers)
+        try:
+            data_dict = json.loads(resp.text)   
+        except:
+            print("Error while attempting to get info about "+company)
+            continue
+
+        concept_dict = dict.fromkeys(concepts_of_interest, None)
+        for concept in concepts_of_interest:
+            company_fin_df = pd.DataFrame.from_dict(data_dict['facts']['us-gaap'][concept]['units']['USD'])
+            concept_dict[concept] = company_fin_df.loc[company_fin_df['form'] == "10-K", ['end','val']].drop_duplicates().drop_duplicates(subset=['end'], keep='first')
+            concept_dict[concept]['end'] = pd.to_datetime(concept_dict[concept]['end'])
+        company_facts_df = pd.concat(concept_dict, axis=0)
+        company_facts_df.reset_index(inplace=True) 
+        company_facts_df.drop(columns=['level_1'], inplace=True)
+        company_facts_df.rename(columns = {'level_0':'concept'}, inplace=True)
+        all_companies_dict[company] = company_facts_df
+
+    return(all_companies_dict)
